8c8cf87dbfe0409c059ada38d5c643071d957ba8
Iyatan Atchoro <iyatan.atchoro@mail.mcgill.ca>
2020-12-03 23:09:54 -0500

    table addition

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,12 @@
 import '../css/Home.css'
+import TableComponent from '../component/TableComponent'
 
 function Home() {
     return (
         <div className="Home">
             We are at home
+            <TableComponent />
+
         </div>
     );
 }
